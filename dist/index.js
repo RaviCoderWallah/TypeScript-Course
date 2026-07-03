99d254d1ced9ad7b2daf9ce87ba0e0f41d6c1c9b
RaviCoderWallah <raviverma05042023@gmail.com>
2026-07-03 18:11:28 +0530

    feat: remove node-modules

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello, ${name}`;
-}
-const username = "Ravi Verma";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
